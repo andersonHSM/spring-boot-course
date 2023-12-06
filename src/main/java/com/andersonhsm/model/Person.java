571ff45392bb28d5be0552fcc99e4990bc2b399c
Anderson Menezes <andersonmenezes.dev@gmail.com>
2023-12-06 08:03:44 -0300

    map Person to table

--- src/main/java/com/andersonhsm/model/Person.java
+++ src/main/java/com/andersonhsm/model/Person.java
@@ -3,12 +3,31 @@ package com.andersonhsm.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false, length = 80)
+
     private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
+
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 100)
     private String gender;
 
     public Person() {
@@ -56,8 +75,10 @@ public class Person implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Person person = (Person) o;
         return Objects.equals(id, person.id)
                 && Objects.equals(firstName, person.firstName)
